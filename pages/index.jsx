bdc868382f904a80decf911711d11143c3532fda
Akhmad Khudri <khudri@binadarma.ac.id>
2025-10-16 05:07:47 +0700

    Add z-10 class to center divs for stacking context

--- pages/index.jsx
+++ pages/index.jsx
@@ -116,7 +116,7 @@ export default function Home() {
         </div>
       </section>
 
-      <div className="relative w-full flex justify-center">
+      <div className="relative w-full flex justify-center z-10">
         <div className="w-full h-[4px] bg-gradient-to-r from-cyan-400 via-purple-500 to-yellow-400 my-6 rounded-full border border-white/20 shadow-lg" />
       </div>
       <motion.section
@@ -146,7 +146,7 @@ export default function Home() {
         )}
       </div>
 
-      <div className="relative w-full flex justify-center">
+      <div className="relative w-full flex justify-center z-10">
         <div className="w-3/4 h-[4px] bg-gradient-to-r from-cyan-400 via-purple-500 to-yellow-400 my-8 rounded-full border border-white/20 shadow-[0_0_25px_rgba(147,51,234,0.6)] opacity-90" />
       </div>
       
