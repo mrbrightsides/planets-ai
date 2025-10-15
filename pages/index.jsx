48ead82eadfc35d7af734e18abd1074deb2b8c22
Akhmad Khudri <khudri@binadarma.ac.id>
2025-10-15 08:46:45 +0700

    Simplify mesh position updates when not hovered
    Refactor mesh position calculations to simplify logic when not hovered.

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,11 +20,11 @@ function FloatingSphere({ index, color, name, link, desc, onHover }) {
 
 useFrame(({ clock }) => {
   const t = clock.getElapsedTime() * speed + angleOffset;
-
-  const hoverFactor = hovered ? 0.1 : 1;
-  mesh.current.position.x = Math.cos(t) * radius * hoverFactor;
-  mesh.current.position.y = Math.sin(t * 0.9) * radius * 0.5 * hoverFactor;
-  mesh.current.position.z = Math.sin(t) * radius * hoverFactor;
+  if (!hovered) {
+    mesh.current.position.x = Math.cos(t) * radius;
+    mesh.current.position.y = Math.sin(t * 0.9) * radius * 0.5;
+    mesh.current.position.z = Math.sin(t) * radius;
+  }
 
   mesh.current.rotation.y += 0.003;
 });
